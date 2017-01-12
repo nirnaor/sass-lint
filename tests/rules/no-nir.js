06cfa9af9bb23325259d71a2cc221c5bdf42c608
Dany Shaanan <danyshaanan@gmail.com>
2017-01-12 16:10:25 +0200

    Add options to rule and expand tests

--- tests/rules/no-nir.js
+++ tests/rules/no-nir.js
@@ -2,98 +2,39 @@
 
 var lint = require('./_lint');
 
-//////////////////////////////
-// SCSS syntax tests
-//////////////////////////////
-describe.only('no-nir - scss', function () {
-  var file = lint.file('no-nir.scss');
+var testCases = [
+  { warningCount: 0, options: { order: [] } },
+  { warningCount: 0, options: {} },
+  { warningCount: 0, options: { order: ['includeWithoutBlock', 'declaration', 'includeWithBlock'] } },
+  { warningCount: 1, options: { order: ['declaration', 'includeWithoutBlock', 'includeWithBlock'] } },
+  { warningCount: 2, options: { order: ['includeWithBlock', 'declaration', 'includeWithoutBlock'] } }
+];
 
-  it('enforce', function (done) {
-    lint.test(file, {
-      'no-nir': 1
-    }, function (data) {
-      lint.assert.equal(0, data.warningCount);
-      done();
+var fileTypes = ['scss', 'sass'];
+
+fileTypes.forEach(function (fileType) {
+  describe.only('no-nir - ' + fileType, function () {
+    var file = lint.file('no-nir.' + fileType);
+
+    testCases.forEach(function (test) {
+      it('' + test.desc, function (done) {
+        lint.test(file, {
+          'no-nir': [1, test.options]
+        }, function (data) {
+          lint.assert.equal(test.warningCount, data.warningCount);
+          done();
+        });
+      });
     });
-  });
 
-  // it('[excludes: all]', function (done) {
-  //   lint.test(file, {
-  //     'no-nir': [
-  //       1,
-  //       {
-  //         'exclude': [
-  //           'test-again',
-  //           'waldo',
-  //           'mq',
-  //           'breakpoint'
-  //         ]
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(0, data.warningCount);
-  //     done();
-  //   });
-  // });
-
-});
-
-//////////////////////////////
-// Sass syntax tests
-//////////////////////////////
-describe.only('mixins before declarations - sass', function () {
-  var file = lint.file('no-nir.sass');
-
-  it('enforce', function (done) {
-    lint.test(file, {
-      'no-nir': 1
-    }, function (data) {
-      lint.assert.equal(0, data.warningCount);
-      done();
+    it('enforce', function (done) {
+      lint.test(file, {
+        'no-nir': 1
+      }, function (data) {
+        lint.assert.equal(0, data.warningCount);
+        done();
+      });
     });
-  });
 
-  //////////////////////////////
-  // Mixins Before Declarations - overwrite
-  //////////////////////////////
-  // it('[excludes: all]', function (done) {
-  //   lint.test(file, {
-  //     'no-nir': [
-  //       1,
-  //       {
-  //         'exclude': [
-  //           'test-again',
-  //           'waldo',
-  //           'mq',
-  //           'breakpoint'
-  //         ]
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(0, data.warningCount);
-  //     done();
-  //   });
-  // });
-  //
-  // //////////////////////////////
-  // // Mixins Before Declarations - exclude certain
-  // // mixin test-again is not excluded
-  // //////////////////////////////
-  // it('[excludes: limited]', function (done) {
-  //   lint.test(file, {
-  //     'no-nir': [
-  //       1,
-  //       {
-  //         'exclude': [
-  //           'waldo',
-  //           'mq',
-  //           'breakpoint'
-  //         ]
-  //       }
-  //     ]
-  //   }, function (data) {
-  //     lint.assert.equal(1, data.warningCount);
-  //     done();
-  //   });
-  // });
+  });
 });
