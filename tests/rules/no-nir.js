359715c47c036fc40018f8583f7ed2fe30c97ac2
Dany Shaanan <danyshaanan@gmail.com>
2017-01-12 13:12:47 +0200

    Print list of nodes to be sorted correctly

--- tests/rules/no-nir.js
+++ tests/rules/no-nir.js
@@ -62,59 +62,59 @@ describe.only('no-nir - scss', function () {
 //////////////////////////////
 // Sass syntax tests
 //////////////////////////////
-describe('mixins before declarations - sass', function () {
-  var file = lint.file('no-nir.sass');
-
-  it('enforce', function (done) {
-    lint.test(file, {
-      'no-nir': 1
-    }, function (data) {
-      lint.assert.equal(5, data.warningCount);
-      done();
-    });
-  });
-
-  //////////////////////////////
-  // Mixins Before Declarations - overwrite
-  //////////////////////////////
-  it('[excludes: all]', function (done) {
-    lint.test(file, {
-      'no-nir': [
-        1,
-        {
-          'exclude': [
-            'test-again',
-            'waldo',
-            'mq',
-            'breakpoint'
-          ]
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(0, data.warningCount);
-      done();
-    });
-  });
-
-  //////////////////////////////
-  // Mixins Before Declarations - exclude certain
-  // mixin test-again is not excluded
-  //////////////////////////////
-  it('[excludes: limited]', function (done) {
-    lint.test(file, {
-      'no-nir': [
-        1,
-        {
-          'exclude': [
-            'waldo',
-            'mq',
-            'breakpoint'
-          ]
-        }
-      ]
-    }, function (data) {
-      lint.assert.equal(1, data.warningCount);
-      done();
-    });
-  });
-});
+// describe.only('mixins before declarations - sass', function () {
+//   var file = lint.file('no-nir.sass');
+//
+//   it('enforce', function (done) {
+//     lint.test(file, {
+//       'no-nir': 1
+//     }, function (data) {
+//       lint.assert.equal(5, data.warningCount);
+//       done();
+//     });
+//   });
+//
+//   //////////////////////////////
+//   // Mixins Before Declarations - overwrite
+//   //////////////////////////////
+//   it('[excludes: all]', function (done) {
+//     lint.test(file, {
+//       'no-nir': [
+//         1,
+//         {
+//           'exclude': [
+//             'test-again',
+//             'waldo',
+//             'mq',
+//             'breakpoint'
+//           ]
+//         }
+//       ]
+//     }, function (data) {
+//       lint.assert.equal(0, data.warningCount);
+//       done();
+//     });
+//   });
+//
+//   //////////////////////////////
+//   // Mixins Before Declarations - exclude certain
+//   // mixin test-again is not excluded
+//   //////////////////////////////
+//   it('[excludes: limited]', function (done) {
+//     lint.test(file, {
+//       'no-nir': [
+//         1,
+//         {
+//           'exclude': [
+//             'waldo',
+//             'mq',
+//             'breakpoint'
+//           ]
+//         }
+//       ]
+//     }, function (data) {
+//       lint.assert.equal(1, data.warningCount);
+//       done();
+//     });
+//   });
+// });
